abc53750a0d31ff60b62d151a8ebe832dd0d27e3
kanisorntin <ocooltv202love@gmail.com>
2025-12-05 17:42:15 +0700

    Add files via upload

--- lab3.c
+++ lab3.c
@@ -4,26 +4,26 @@ int *GetSet( int *data, int *num );
 
 int main(){
     int *data, num;
-    data = GetSet(data ,&num);
+    data = GetSet( &num ,data );
     return 0;
 }
 
-int *GetSet(int *data,int *num){
-    printf("How many number ? : ");
+int *GetSet(int *data, int *num){
         if(scanf("%d", num) != 1){
-            printf("No have number!\n");
-            return 0;
+            printf("Error!! not a number\n");
     }
-
     for(int i = 0; i < *num; i++){
-        printf("Enter Number %d: ", i+1);
+        printf("Number %d is : ", i+1);
         if(scanf("%d",&data[i]) != 1){
-            printf("No have number!\n");
+            printf("Error!! not a number\n");
         }
     }
-
     printf("\nYour number is:\n");
     for(int i = 0; i < *num; i++){
-        printf("%d",data[i]);
+    printf("%d",data[i]);
     }
+    return 0;
 }
+
+
+    
